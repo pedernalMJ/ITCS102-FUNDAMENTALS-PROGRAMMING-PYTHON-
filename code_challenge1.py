3ce8ce44f401b8127a982a459f8e47308a3fb0af
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-08-11 14:26:20 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,14 @@
+name=input("Whats your name: ")
+
+diamond = (
+    "\n\t\t\t\t*\t\t\t\t"
+    "\n\t\t\t*\t\t*\t\t\t"
+    "\n\t\t*\t\t\t\t*\t\t"
+    "\n\t*\t\t       Hi\t\t\t*\t\t"
+    f"\n*\t\t\t      {name}\t\t\t\t*\t"
+    "\n\t*\t\t\t\t\t\t*\t"
+    "\n\t\t*\t\t\t\t*\t\t"
+    "\n\t\t\t*\t\t*\t\t\t"
+    "\n\t\t\t\t*\t\t\t\t"
+)
+print(diamond)
