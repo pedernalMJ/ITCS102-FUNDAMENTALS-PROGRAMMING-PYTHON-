3ced92a60887c2f880a9b272b73af52d8c5defe2
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-08-18 11:25:55 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,10 @@
+identity = "mark"
+pin  = "0000"
+
+#print(identity == "mark")
+#print(pin == "0000")
+#print(identity == "mark") and (pin == "0000")
+#print(identity == "mark") or (pin == "0001")
+print(not((identity == "mark") or (pin == "0001")))
+
+
