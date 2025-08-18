5a31044b90374479ac21d47b97e5387c099067d8
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-08-18 11:25:23 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,17 @@
+a = 5
+
+print("The value of a is ", a)
+
+a += 5
+print("The value of a is ", a)
+
+a -= 20
+print("The value of a is ", a)
+
+a *= 6 
+print("The value of a is ", a)
+
+a /= 10 
+print("The value of a is ", a)
+
+
