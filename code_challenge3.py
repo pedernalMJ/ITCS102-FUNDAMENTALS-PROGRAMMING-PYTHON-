891013a0c5ce4ff799dd613951fff78caa377e8f
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-08-27 10:57:34 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,10 @@
+username = "admin"
+password = "12345"
+
+user = input("Enter username:")
+pwd = input("Enter password:")
+
+if user == username and pwd == password :
+	print("Access Granted")
+else:
+	print("Access Denied")
