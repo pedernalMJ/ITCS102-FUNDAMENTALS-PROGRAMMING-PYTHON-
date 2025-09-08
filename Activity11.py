545eed8ab7837c3047dfe9f02ba8b7de14c6fe48
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-09-08 11:46:31 +0800

    Update Activity11.py

--- Activity11.py
+++ Activity11.py
@@ -3,15 +3,15 @@ temp = eval(input("Enter temperature --> "))
 if temp < 1: 
     print("Temperature freezing cold")
 elif temp >= 1 and temp <= 20:
-    print("Temperature outside cold")
+    print("Temperature cold")
 elif temp >= 21 and temp <= 30:
-    print("Temperature outside lukewarm")
+    print("Temperature lukewarm")
 elif temp >= 31 and temp <= 40:
-    print("Temperature outside warm")
+    print("Temperature warm")
 elif temp >= 41 and temp <= 50:
-    print("Temperature outside hot")
+    print("Temperature hot")
 elif temp >= 51:
-    print("Temperature outside boiling hot")
+    print("Temperature above boiling hot")
 else:
     print("Invalid temperature")
   
