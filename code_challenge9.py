ee2ca72d58891d6ac5fde5630509688ac0e9df4c
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-09-17 10:28:40 +0800

    Add files via upload

---
+++ code_challenge9.py
@@ -0,0 +1,11 @@
+print("⏳ COUNTDOWN TIMER SIMULATOR")
+nim = eval(input("Enter the starting number for countdown: "))
+
+
+
+print("\nCountdown started:")
+for nim in range(nim,0,-1):
+    print(nim)
+print("Liftoff!")
+
+
