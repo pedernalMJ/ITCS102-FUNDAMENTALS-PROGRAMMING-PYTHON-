9d198d9a11d0420d513e45732011481009d63830
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-09-19 11:55:18 +0800

    Update activity15.py

--- activity15.py
+++ activity15.py
@@ -5,11 +5,9 @@
 #print("my name is", ign1," ", ign2," ", ign3)
 #print(f"my name is {name1} {name2} {name3}")
 
-odd = 0
+odd_sum = 0
 for i in range(1, 11, 1):
     num = eval(input(f"{i} - Enter a number - - > "))
-
     if num % 2 == 1:
-        odd += num
-
-print(f"The sum of all the ODD numbers given is:", {odd})
+        odd_sum += num
+print(f"The sum of all the ODD numbers given is:", {odd_sum})
