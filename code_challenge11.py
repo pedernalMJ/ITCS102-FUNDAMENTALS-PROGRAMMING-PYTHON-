1700ede7005ce893697324504970e9169e3a98f1
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-09-22 13:07:48 +0800

    Add files via upload

---
+++ code_challenge11.py
@@ -0,0 +1,18 @@
+
+for i in range(1, 11, 1):
+    for x in range(10, i, -1):
+        print("",end=" ")
+    for z in range(1,i,-1):
+        print("*", end = " ")
+    for y in range(1, i, 1):
+        print("*", end= " ")
+    print()
+
+for i in range(10, 0, -1):                # bottom half
+    for x in range(10, i, -1):
+        print(" ", end="")
+    for z in range(1, i, -1):
+        print("*", end=" ")
+    for y in range(1, i, 1):
+        print("*", end=" ")
+    print()
