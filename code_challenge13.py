09ff1b8b3c8fe58bec3ea197f1329a763b7ca182
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-10-02 05:53:10 +0800

    Add files via upload

---
+++ code_challenge13.py
@@ -0,0 +1,54 @@
+for i in range(1, 5, 1):
+    for x in range(19, i, -1):
+        print("",end=" ")
+    for z in range(1,i,-1):
+        print("*", end = " ")
+    for y in range(0, i, 1):
+        print("*", end= " ")
+    print()
+for i in range(5, 0, -1):                
+    for x in range(19, i, -1):
+        print(" ", end="")
+    for z in range(1, i, -1):
+        print("*", end=" ")
+    for y in range(0, i, 1):
+        print("*", end=" ")
+    print()
+for i in range(1, 11, 1):
+    for y in range(10, i, -1):
+        print(" ", end=" ")
+    for x in range(1, i, 1):
+        print("*", end=" ")
+    for z in range(0, i, 1):
+        print("*", end=" ")
+    print()
+
+for i in range(1, 11, 1):
+    for y in range(10, i, -1):
+        print(" ", end=" ")
+    for x in range(1, i, 1):
+        print("*", end=" ")
+    for z in range(0, i, 1):
+        print("*", end=" ")
+    print()
+for i in range(1, 11, 1):
+    for y in range(10, i, -1):
+        print(" ", end=" ")
+    for x in range(1, i, 1):
+        print("*", end=" ")
+    for z in range(0, i, 1):
+        print("*", end=" ")
+    print()
+for i in range(4):  
+    for y in range(8):  
+        print(" ", end=" ")
+    for x in range(3):  
+        print("*", end=" ")
+    print()
+
+
+    
+
+
+
+
