6d036910799dd3935629b1ca58007140fa2a05ca
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-10-15 20:24:37 +0800

    Add files via upload

---
+++ code_challenge14.py
@@ -0,0 +1,22 @@
+name =input("Enter name -->")
+print(f"Hi ! what is your name -->{name}")
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("ODD number compiler, type 0 to terminate the loop")
+oddsum = 0
+odd = ""
+t = True
+while t == True:
+    num = eval(input("Enter number-->"))
+    if num == 0:
+        print("terminated loop")
+        break
+    if num % 2 == 1:
+        oddsum += num
+        odd += str(num) +","
+        print("ODD Number detected")
+        continue
+    else:
+        print("invalid number")
+        continue
+print(f"Hi {name}, The sum of all ODD number is {oddsum}")
+print(f"All the ODD numbers you input is {odd}")
