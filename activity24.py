62eb0d9e6531ea45a7dfb2f987dcdc65307bccf6
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-11-07 11:07:41 +0800

    Add files via upload

---
+++ activity24.py
@@ -0,0 +1,12 @@
+def greeter(name):
+  print(f"Hi {name}, kamusta kana? ")
+
+def summation(x):
+  sum = 0
+  for y in range(x,0,-1):
+    sum += y
+  print(f"The sum of {x} is {sum}")
+
+greeter("juven")
+greeter("pedernal")
+summation(18)
