001cbf70c71109716eb2a74816e2ace2ae3e64e2
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-11-18 18:47:26 +0800

    Update code_challenge16.py

--- code_challenge16.py
+++ code_challenge16.py
@@ -9,18 +9,18 @@ student_records = {}
 while True: 
     print("Select from the options: \nA - Add Information\nB - Search a Record\nC - Delete a Record\nD - Modify a Record\nE - Exit\nP - Print All ")
     
-    choice = input("Your Choice - - - - > ")
+    choice = input("Your Choice ---> ").upper()
 
     if choice == 'A':
         print("ADDING STUDENT INFORMATION")
         print("= = = = = = = = = = = = = = =")
         
-        search_code = input("Key search for this student - - - > ").upper()
-        first_name = input("Type the Student's First name - - > ").upper()
-        last_name = input("Type the Student's Last name - - - > ").upper()
-        course = input("Type the Student's Course - - - > ").upper()
-        email = input("Type the Student's email address - - - > ")
-        isSingle= input("Are you Single or Married? (S / M) - - - >")
+        search_code = input("Key search for this student ---> ").upper()
+        first_name = input("Type the Student's First name ---> ").upper()
+        last_name = input("Type the Student's Last name ---> ").upper()
+        course = input("Type the Student's Course ---> ").upper()
+        email = input("Type the Student's email address ---> ")
+        isSingle= input("Are you Single or Married? (S / M)--->").upper()
 
         student_records = {search_code : [first_name, last_name, course, email, isSingle]}
         print("DATA SAVED")
@@ -39,20 +39,20 @@ while True:
                 for i in student_records[code]:
                     print(i)
 
-            else: print("NO RECORD FOUND . . . ")
+            else: print("No Recrod . . . ")
         continue
     elif choice =='C':
         pass
         continue
     elif choice == 'D':
-        print("EDITING EXISTING RECORD . . . ")
+        print("Editing . . . ")
         continue
     elif choice == 'E':
         print("SYSTEM EXIT")
         break
     elif choice == "P":
         for i,j in student_records.items():
-            print(f"Code - {i} STORED INFORMATION - - - > {j}")
+            print(f"Code - {i} stored information - - - > {j}")
     else:
-        print("\nINVALID CHOICE, PLEASE RE-ENTER YOUR CHOICE")
+        print("\nInvalid choice")
         continue
