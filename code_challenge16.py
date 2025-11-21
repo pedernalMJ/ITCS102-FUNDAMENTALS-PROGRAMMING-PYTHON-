e824fd0f9bb21e47356a6a8aae52ab9162a60625
pedernalMJ <pedernalmarkjuben@gmail.com>
2025-11-21 11:38:08 +0800

    Add files via upload

--- code_challenge16.py
+++ code_challenge16.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 os.system("cls")
 print("STUDENT INFORMATION SYSTEM")
@@ -7,7 +8,7 @@ print("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 student_records = {}
 
 while True: 
-    print("Select from the options: \nA - add student reocerd\nB - print all records\nC - search records\nD - delete record\nE - Edit student record\nF - exprot data\nG - exit ")
+    print("Select from the options: \nA - add student reocerd\nB - print all records\nC - search records\nD - delete record\nE - Edit student record\nF - exprot data\nG - Import\nX - EXit ")
     
     choice = input("Your Choice ---> ").upper()
 
@@ -69,12 +70,61 @@ while True:
             print("=====================")
 
         continue
+    elif choice == "E":
+        print("Editing / Modify")
+
+        search_id= input("input id to search-->").upper()
+        for id in student_records.keys():
+            if search_id in student_records.keys():
+                print("=====================")
+                print("\n\nRecord found")
+                print("=====================")
+                for i in student_records[search_id]:
+                    print(f"-- {i}")
+        else:
+            print("no record found")
+
+        first_name = input("Type the Student's First name ---> ").upper()
+        last_name = input("Type the Student's Last name ---> ").upper()
+        course = input("Type the Student's Course ---> ").upper()
+        email = input("Type the Student's email address ---> ")
+        isSingle= input("Are you Single or Married? (S / M)--->").upper()
+
+        student_records[search_id][0]= first_name
+        student_records[search_id][1]= last_name
+        student_records[search_id][2]= course
+        student_records[search_id][3]= email
+        student_records[search_id][4]= isSingle
+
+        print("Data updated")
+
+
+
     elif choice == 'F':
-        print("export")  
+        os.system("cls")
+        print("export") 
+
+        with open("student_record.json","w") as new_file:
+            json.dump(student_records,new_file,indent=4)
+            print("DATA EXPORTED TO JASON")
+            continue
     elif choice == "G":
-        print("exiting")
-        break
+        os.system("cls")
+        print("Import student records")
         
+        with open("student_record.json","r") as new_file:
+            student_json = json.load(new_file)
+            print("Data Exported to json")
+
+        
+    
+    elif choice == "x":
+            
+            print("existing")
+            break
     else:
-        print("\nInvalid choice")
+        print("invalid")
         continue
+
+        
+    
